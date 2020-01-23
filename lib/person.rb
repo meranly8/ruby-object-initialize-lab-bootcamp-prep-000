3b268e89037d5214b99c95ed2e8e41e93a0d1777
Michelle Ranly <meranly8@gmail.com>
2020-01-23 22:02:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,5 @@ class Person
   def initialize(persons_name)
     @name = persons_name
   end
-  
-  def name=(persons_name)
-    @name = persons_name
-  end
-  
-  def name 
-    @name
-  end
+ 
 end
